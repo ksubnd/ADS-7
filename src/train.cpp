8059999321dac7948ee66d588b627fc06dc97739
ksubnd <ksubnd@gmail.com>
2026-05-20 22:55:11 +0300

    train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,38 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+#include <iostream>
+
+void Train::addCar(bool light) {
+  if (first == nullptr) {
+    first = createCar(light);
+    first->next = first;
+    first->prev = first;
+  } else {
+    Car* item = createCar(light);
+    item->prev = first->prev;
+    item->next = first;
+    first->prev->next = item;
+    first->prev = item;
+  }
+}
+
+int Train::getLength()
+{
+    first->light = true;
+    Car* temp = first->next;
+    int count = 1;
+    while (temp != first) {
+        if (temp->light == true) {
+            countOp += (count * 2);
+        }
+        temp = temp->next;
+        count++;
+    }
+    countOp += (count * 2);
+    return count;
+}
+
+int Train::getOpCount()
+{
+  return countOp;
+}
