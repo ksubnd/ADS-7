75c058bbd93adc743e8a34c003b37fc2e5a6e21b
ksubnd <ksubnd@gmail.com>
2026-05-21 00:05:43 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,9 @@
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
-
-  while (count--)
-    train.addCar(false);
-
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
-  return 0;
-
-  int size[] = { 500, 800, 1000,1500, 2000,4000, 5000,6000,8000,10000,25000,30000,40000,50000,60000,120000,150000, 200000,250000,300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000, 0 };
+  int size[] = { 500, 800, 1000, 1500, 2000, 4000, 5000,6000, 8000, 10000,
+    25000, 30000, 40000, 50000, 60000, 120000, 150000, 200000,250000,300000, 
+    400000, 500000, 600000, 700000, 800000, 900000, 1000000, 0 };
   for (int i = 0; size[i]; i++) {
     clock_t begin1, end1, begin2, end2, begin3, end3;
     clock_t t1, t2, t3;
@@ -24,8 +16,8 @@ int main() {
     while (j--)
       train1.addCar(false);
     begin1 = clock();
-    int count1L = train1.getLength();
-    int count1C = train1.getOpCount();
+    train1.getLength();
+    train1.getOpCount();
     end1 = clock();
     j = size[i];
 
@@ -33,8 +25,8 @@ int main() {
     while (j--)
       train2.addCar(true);
     begin2 = clock();
-    int count2L = train2.getLength();
-    int count2C = train2.getOpCount();
+    train2.getLength();
+    train2.getOpCount();
     end2 = clock();
     j = size[i];
 
@@ -46,8 +38,8 @@ int main() {
         train3.addCar(false);
     }
     begin3 = clock();
-    int count3L = train3.getLength();
-    int count3C = train3.getOpCount();
+    train3.getLength();
+    train3.getOpCount();
     end3 = clock();
     t1 = end1 - begin1;
     t2 = end2 - begin2;
